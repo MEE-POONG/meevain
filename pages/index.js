0a20a601aeaf3a8dde0ab4b76b152459483067b0
Jame2455 <93962729+Jame2455@users.noreply.github.com>
2022-01-27 13:55:50 +0700

    CreateGroup

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import React from "react"
+import ModalCreateGroup from "../components/modalCreateGroup"
 import Login from "./login"
 export default function Home() {
   return (
-<>  </>
+<div>
+  
+  <ModalCreateGroup/>
+</div>
  
 
   )
