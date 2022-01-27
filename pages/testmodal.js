f3bbac4fb4598a315c5b1ad948ffb03b94229173
Jame2455 <93962729+Jame2455@users.noreply.github.com>
2022-01-27 12:14:48 +0700

    ...

--- pages/testmodal.js
+++ pages/testmodal.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import TableVain from '../components/tableVain';
+// import TopPageAdmin from '../components/topPageAdmin';
 
 
 
 export default function Testmodal() {
   return (
     <div>
-        
+        <TableVain/>
     </div>
   );
 }
