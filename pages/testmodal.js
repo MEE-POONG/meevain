3b76d261f1fe2a5f36294ab98ada72e5877a963c
Jame2455 <93962729+Jame2455@users.noreply.github.com>
2022-01-27 13:00:14 +0700

    66

--- pages/testmodal.js
+++ pages/testmodal.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
-
+import ModalCreateGroup from '../components/modalCreateGroup';
 
 export default function Testmodal() {
   return (
     <div>
-        
+        <ModalCreateGroup/>
     </div>
   );
 }
